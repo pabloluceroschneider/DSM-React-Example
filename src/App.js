0bc5df237d8246a9606b5e05a890278226e3815d
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-09 13:33:04 -0300

    sab9may

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Welcome = props => {
+  const { name } = props;
+  const [ hello, setHello ] = useState(true)
+
+  return (
+    <div>
+      <button onClick={ () => {setHello(!hello)} }> 
+        { hello ? "Say Goodbye" : "Say Hi"}
+      </button>
+      <br/><br />
+      {
+        name ? 
+        <>
+          <span> { hello ? "Hi" : "Bye"} </span>
+          <span> { name } </span>
+        </>
+        : null
+      }
+    </div>
+  )
+}
+
+const App = () => {
+  const [name, setName] = useState(null)
+  const inputRef = useRef(null)
+
+  const handleClick = () => {
+    inputRef.current.focus();
+  }
+
+  const handleInput = e => {
+    e.preventDefault();
+    setName( e.target.value );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Focus on input</button> <br />
+        <input onChange={handleInput} ref={inputRef} placeholder="Name"></input> <br />
+        <Welcome name={name} />
       </header>
     </div>
   );
